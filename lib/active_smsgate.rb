d23cdeed1e76c389e279027c4dd845e4f68e53ee
Pechnikov Maxim <parallel588@gmail.com>
2010-05-08 20:29:48 +0600

    Получение статуса по отправки смс для amegainform

--- lib/active_smsgate.rb
+++ lib/active_smsgate.rb
@@ -6,3 +6,30 @@ require 'httparty'
 require 'nokogiri'
 require 'digest'
 require "zlib"
+
+require "active_smsgate/gateway"
+Dir.glob(File.join('active_smsgate', "gateway", "*.rb")).each do |gw|
+  require gw
+end
+
+module ActiveSmsgate
+  VERSION = "0.0"
+  class << self
+
+    def log message
+      puts "lid ---------------"
+      # logger.info("[active_smsgate] #{message}") #if logging?
+    end
+
+    def logger #:nodoc:
+      # # ActiveRecord::Base.logger
+      # FILE = "sms_send.log"
+      # $logger = Logger.new(FILE,3,5000000)
+    end
+
+    def logging? #:nodoc:
+      true #options[:log]
+    end
+
+  end
+end
--- lib/active_smsgate/gateway/amegainform.rb
+++ lib/active_smsgate/gateway/amegainform.rb
@@ -54,7 +54,56 @@ module ActiveSmsgate #:nodoc:
       def deliver(options = { }); end
 
       # Получение данных и статусов сообщений
-      def reply(options = { }); end
+      # sms_id - ид смс
+
+
+      # Возвращаемые данные
+      # -------------------------------------------------------------------
+      # SMS_ID - ID сообщения
+      # SMS_GROUP_ID - ID рассылки сообщений
+      # SMSTYPE - тип сообщения
+      # CREATED - дата и время создания сообщения
+      # AUL_USERNAME - Имя пользователя создавшего сообщение
+      # AUL_CLIENT_ADR - IP адрес пользователя создавшего сообщение
+      # SMS_SENDER - Имя отправителя сообщения
+      # SMS_TARGET - Телефон адресата
+      # SMS_RES_COUNT - Кол-во единиц ресурсов на данное сообщение
+      # SMS_TEXT - Текст сообщения
+      # SMSSTC_CODE - Код статуса доставки сообщения
+      # -- queued - сообщение в очереди отправки
+      # -- wait -	передано оператору на отправку
+      # -- accepted -	сообщение принято оператором, но статус доставки неизвестен
+      # -- delivered -сообщение доставлено
+      # -- not_delivered - сообщение не доставлено
+      # -- failed - ошибка при работе по сообщению
+      # SMS_STATUS - Текстовое описание статуса доставки сообщения
+      # SMS_CLOSED - [0,1] 0 - сообщения находится в процессинге.
+      #                    1 = работа по отправке сообщения завершена
+      # SMS_SENT - [0,1] 0 - сообщение не отослано. 1 = сообщение отослано успешно
+      # SMS_CALL_DURATION - Время,
+      #              в течение которого было установлено соединение для отправки сообщения.
+      # SMS_DTMF_DIGITS - Что пользователь нажимал в сеансе разговора (для SENDVOICE (в разработке))
+      # SMS_CLOSE_TIME - Время завершения работы по сообщению.
+
+      def reply(sms_id)
+        @options = { :action => "status", :sendtype => "SENDSMS", :sms_id => sms_id }
+        @response = self.class.post("#{uri}/sendsms", :query => @options.merge(auth_options))
+
+        if @response.code == 200
+          xml = Zlib::GzipReader.new( StringIO.new( @response ) ).read
+          doc = Nokogiri::XML(xml)
+          @attr = { }
+          doc.at("//MESSAGES//MESSAGE").each {|x, t| attr[x.downcase.to_sym] = t}
+          doc.at("//MESSAGES//MESSAGE").children.
+            map { |t| attr[t.name.downcase.to_s] = t.inner_html unless t.blank? }.compact
+          @attr
+        else
+          raise
+        end
+      rescue
+        nil
+      end
+
 
       private
       # Возвращает параметры для авторизации
